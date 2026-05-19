292663e2a60259a76442a007cab8b890a5524d44
COFFEE21 <joaopedrorbruno@gmail.com>
2026-05-19 19:56:50 -0300

    Estruturei as classes

--- Main.py
+++ Main.py
@@ -1,34 +1,55 @@
 import os
+from os import write
 
-file_path = "/home/coffee/PycharmProjects/PythonProject/Txt-Editor/Data.txt"
-txt_data = "Eu quero escrever alguma coisa aqui"
 
-def check(path):
+class File:
 
-    if os.path.isfile(path):
-        print("É um camihno válido para um arquivo")
+    file_path = "/home/coffee/PycharmProjects/PythonProject/Txt-Editor/Default_Local/Output.txt"
+    txt_data = "Eu quero escrever alguma coisa aqui"
 
-    elif os.path.isdir(path):
-        print("É um caminho válido para um diretório")
+    def __init__(self,path = "/home/coffee/PycharmProjects/PythonProject/Txt-Editor/Default_Local/Output.txt"):
+       self.path = path
 
-    else:
-        print("Não é um caminho válido")
+       with open(path , "w") as file:
+           write("")
 
-def escrever(path):
-    with open(path, mode ="w") as file:
-        file.write(txt_data)
-        print("escrita do arquivo realizada")
 
-def ler(path):
-    try:
+    @staticmethod
+    def check(path):
+
+         if os.path.isfile(path):
+            print("É um camihno válido para um arquivo")
+
+         elif os.path.isdir(path):
+            print("É um caminho válido para um diretório")
+
+         else:
+            print("Não é um caminho válido")
+
+    def escrever(self):
+        path = self.path
+        data = f"\n{input("O que você deseja escrever no arquivo?")}"
+        with open(path, mode ="a") as file:
+            file.write(data)
+            print("escrita do arquivo realizada")
+
+    def ler(self):
+      path = self.path
+      try:
         with open(path, "r") as file:
             content = file.read()
             return content
 
-    except FileNotFoundError:
+      except FileNotFoundError:
         print("Arquivo não encontrado!")
 
+    def resetar(self):
+        path = self.path
+        with open(path ,"w") as file:
+            file.write("")
 
 
 
-escrever(file_path)
+
+
+
